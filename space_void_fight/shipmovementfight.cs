45eeb71e5bca1ceb00075762730ffdeda6851129
maltuin <margai_m@etna-alternance.net>
2014-04-30 17:12:00 +0200

    First commit

---
+++ space_void_fight/shipmovementfight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shipmovementfight : MonoBehaviour {
+	private GameObject controler;
+	public GameObject controler1;
+	public GameObject controler2;
+	public int ship;
+	// Use this for initialization
+	void Start () {
+		if (ship == 1) {
+			controler = (GameObject)Instantiate (controler1, new Vector3 (-51.22791f, 0.6604357f, -0.3738514f), transform.rotation);
+			controler.transform.eulerAngles = new Vector3 (0, 90, -90);
+		} else if (ship == 2) {
+			controler = (GameObject)Instantiate (controler2, new Vector3(-56.32072f,-0.3880819f,-1.104746f), transform.rotation);
+			controler.transform.eulerAngles = new Vector3 (0, 90, 90);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ space_void_transition/Shipmovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Shipmovement : MonoBehaviour
+{
+		public float speed;
+		public int stop;
+		public int i;
+		private GameObject controler;
+		public GameObject controler1;
+		public GameObject controler2;
+		public int ship;
+
+		void Start ()
+		{
+				if (ship == 1) {
+						controler = (GameObject)Instantiate (controler1, new Vector3 (-18.69715f, 7.855296f, -42.20232f), transform.rotation);
+						controler.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
+						controler.transform.eulerAngles = new Vector3 (11, 70, 0);
+				} else if (ship == 2) {
+						controler = (GameObject)Instantiate (controler2, new Vector3(-20.69715f,8.855296f,-42.20232f), transform.rotation);
+						controler.transform.localScale = new Vector3 (1, 1, 1);
+						controler.transform.eulerAngles = new Vector3 (20, 90, 0);
+						speed -=2;
+				}
+
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+				if (i < stop) {
+						controler.transform.position += controler.transform.forward * speed * Time.deltaTime;
+						i += 1;
+				}
+		}
+}
